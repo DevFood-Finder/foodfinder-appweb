af99d6d45dd543fc50466d84e8feba4495617c41
Carlos Chávez <89101032+CarlosChavez19@users.noreply.github.com>
2024-05-29 20:09:44 -0500

    feat: restaurant-page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantPageComponent } from './idk/pages/restaurant-page/restaurant-page.component';
+import { MatIconModule } from "@angular/material/icon";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle
   ],
   providers: [],
   bootstrap: [AppComponent]
